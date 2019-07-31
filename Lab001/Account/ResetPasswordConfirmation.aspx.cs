e34dcfd55f1c5f400a2ff058bd6273a9f989cec9
Khanh Quoc <khanhquochts@gamil.com>
2019-07-31 10:52:37 +0700

    Add project files.

---
+++ Lab001/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Lab001.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Lab001/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab001.Startup))]
+namespace Lab001
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
